79ee407d143e35d5018bc9fe4e53eb3878c8011f
Malpropre <malpropremarsel@gmail.com>
2021-08-06 09:10:39 +0700

    Update app.js

--- themes/light/app.js
+++ themes/light/app.js
@@ -26,12 +26,12 @@ function init() {
     // 搜索
     var search_bar = `
                     <div class="titleBar_item search_bar">
+                        <form class="searchBar_form" method="get" action="/${cur}:search">
+                            <input type="text" name="q" placeholder="Search in current drive" value="${search_text}" />
+                        </form>
                         <a class="titleBar_link searchBar_link" onclick="if($('.search_bar').hasClass('searchBar_link') && $('.searchBar_form>input').val()) $('.searchBar_form').submit();">
                             <i class="mdui-icon material-icons"></i>
                         </a>
-                        <form class="searchBar_form titleBar_exhibit" method="get" action="/${cur}:search">
-                            <input type="text" name="q" placeholder="Search in current drive" value="${search_text}" />
-                        </form>
                     </div>`;
     // 盘
     var pan_bar = `
